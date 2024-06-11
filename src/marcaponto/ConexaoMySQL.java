43f73c1ceeda48847e238c7eacc5e28c496ec718
vbl10 <99830114+vbl10@users.noreply.github.com>
2024-06-11 16:07:21 -0300

    Adicionar arquivos

---
+++ src/marcaponto/ConexaoMySQL.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marcaponto;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+/**
+ *
+ * @author sp300lab
+ */
+public class ConexaoMySQL {
+    private static String url = "jdbc:mysql://db4free.net:3306";
+    private static String usuario = "projeto_java";
+    private static String senha = "senhadobanco";
+    
+    static Connection conectarBanco() throws SQLException {
+        return DriverManager.getConnection(url, usuario, senha);
+    }
+}
